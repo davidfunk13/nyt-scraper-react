fcdc855755fd3456536765bbd494d2ac0e9ce6d2
Dave Funk <davefunk135@gmail.com>
2018-04-22 00:22:22 -0700

    backslash was at the end of <component/> instead of </component> spent way too much time looking for what was causing that. offically done witht he grid. moving on to making other componenets and filling mok database to print to the page for testing.

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
-
+import { Container, Row, Column } from '../../components/BootstrapGrid'
 class Home extends Component {
+    // state= {
+
+    // }
 
     render() {
         return (
-            <div className='sdasd'>asdasd</div>
-        )
+            <Container>
+                <Row>
+                    <Column>
+                    <div className='greetings'>yo yo whats uuuuuup got it </div>
+                    </Column>
+                </Row>
 
+            </Container>
+
+        );
     }
 }
 
